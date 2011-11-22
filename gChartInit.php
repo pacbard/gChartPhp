a27a3dab8d2e3f30dc364b468e74c60654ae776c
Ramprasad Rajendran <r.ramprasad@gmail.com>
2011-11-22 22:49:19 +0530

    added a classloader and init that registers the classloader for easy use

---
+++ gChartInit.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * gChartPhp init script.
+ * Sets up include paths based on the directory this file is in.
+ * Registers an SPL class autoload function.
+ * 
+ * Taken from https://github.com/pda/pheanstalk/blob/master/pheanstalk_init.php and modified
+ */
+
+$gChartClassRoot = dirname(__FILE__);
+require_once($gChartClassRoot. '/ClassLoader.php');
+
+gChart_ClassLoader::register($gChartClassRoot);
